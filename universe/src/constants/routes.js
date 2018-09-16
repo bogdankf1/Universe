7dd61b898488eaff468f4dd68f3cd51472a8cafa
Bogdan <bogdankf1@gmail.com>
2018-09-16 20:45:20 +0300

    Implemented client-to-server basic requests setup

--- universe/src/constants/routes.js
+++ universe/src/constants/routes.js
@@ -1,5 +1,6 @@
-export default {
+export const ROUTES = {
     APP: '/',
     HOME: '/home',
-    STOCK_MARKET_TOOLS: '/stock-market-tools'
+    STOCK_MARKET_TOOLS: '/stock-market-tools',
+    STOCK_MARKET_TOOL: '/stock-market-tools/tool/:tool?'
 }
