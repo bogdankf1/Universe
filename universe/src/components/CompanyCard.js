a688e414f772bf5ec209afbff38c6657bb5b5747
Bogdan <bogdankf1@gmail.com>
2018-10-11 01:20:51 +0300

    Added stocks loading for each selected company

---
+++ universe/src/components/CompanyCard.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import { Paper, Typography } from '@material-ui/core'
+import { TOOLS } from '../constants/ActionTypes';
+
+const styles = {
+  toolsContainer: {
+    width: '100%',
+    padding: '20px'
+  },
+  cardPaper: {
+    minWidth: 150,
+    minHeight: 40,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer'
+  }
+}
+
+class CompanyCard extends Component {
+  selectCompany = () => {
+    const { dispatch, company } = this.props
+
+    dispatch({
+      type: TOOLS.SET_COMPANY_NAME,
+      payload: company
+    })
+  }
+  render() {
+    const { company = {}, classes } = this.props
+    const { cardPaper } = classes
+
+    return (
+      <div onClick={this.selectCompany}>
+        <Paper elevation={1} className={cardPaper}>
+          <Typography variant="h5" component="h3">
+            {company.companyName}
+          </Typography>
+        </Paper>
+      </div>
+    )
+  }
+}
+
+CompanyCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(connect(
+  state => ({
+
+  })
+)(CompanyCard))
--- universe/src/components/ToolStocks.js
+++ universe/src/components/ToolStocks.js
@@ -6,6 +6,8 @@ import { Query } from 'react-apollo'
 import { loadStocksList } from '../graphql/queries/stocks'
 import { DEFAULT_LOADED_STOCKS } from '../constants/app'
 import { TOOLS } from '../constants/ActionTypes'
+import CompanyCard from './CompanyCard'
+import { Grid } from '@material-ui/core'
 
 const styles = {
   toolsContainer: {
@@ -17,30 +19,56 @@ const styles = {
 class ToolStocks extends Component {
   handleLoadedStocksData = (data) => {
     const { dispatch } = this.props
-    console.log(Object.entries(data))
-    const companiesNames = Object.entries(data).map((item) => {
-      return item[1].quote.companyName
-    })
+    const companiesList = Object.entries(data).map(item => {
+        return {
+          symbol: item[1].quote.symbol,
+          companyName: item[1].quote.companyName
+        }
+      })
+
     dispatch({
       type: TOOLS.SAVE_DEFAULT_STOCKS_NAMES_LIST,
-      payload: companiesNames
+      payload: companiesList
     })
-    console.log(companiesNames)
   }
   render() {
+    const { companiesList, classes, selectedCompany } = this.props
+
     return (
       <div>
-        <Query query={loadStocksList} variables={{ id: DEFAULT_LOADED_STOCKS.join(",") }}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>
-            if (error) return <p>Error :(</p>
-            
-            this.handleLoadedStocksData(JSON.parse(data.stocks.list))
-            // console.log(JSON.parse(data.stocks.list))
+        {companiesList.length ? 
+          <Grid container justify={'center'} alignItems={'center'} spacing={16}>
+            {companiesList.map((company, idx) =>
+              <Grid item>
+                <CompanyCard company={company} key={idx} />
+              </Grid>
+            )}
+          </Grid> :
+          <Query query={loadStocksList} variables={{ id: DEFAULT_LOADED_STOCKS.join(",") }}>
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>
+              if (error) return <p>Error :(</p>
+              
+              this.handleLoadedStocksData(JSON.parse(data.stocks.list))
 
-            return data.stocks.list
-          }}
-        </Query>
+              return 'Stocks data loaded successfully!'
+            }}
+          </Query>
+        }
+        <div className="stocks-graph-container">
+          {selectedCompany.companyName ?
+            <div>
+              <Query query={loadStocksList} variables={{ id: selectedCompany.symbol }}>
+                {({ loading, error, data }) => {
+                  if (loading) return <p>{`Loading ${selectedCompany.companyName} stocks data...`}</p>
+                  if (error) return <p>{`Error :(`}</p>
+
+                  return data.stocks.list
+                }}
+              </Query>
+            </div> : null
+          }
+        </div>
       </div>
      ) 
   }
@@ -52,6 +80,8 @@ ToolStocks.propTypes = {
 
 export default withStyles(styles)(connect(
   state => ({
-    toolName: state.tools.toolName
+    toolName: state.tools.toolName,
+    companiesList: state.tools.defaultStocks || [],
+    selectedCompany: state.tools.selectedCompany || {}
   })
 )(ToolStocks))
